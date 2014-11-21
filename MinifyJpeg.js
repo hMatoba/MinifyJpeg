484e2b7242adacf30ca00247351715af1a5d46b5
hMatoba <g838.mahiroo@gmail.com>
2014-11-21 10:59:45 +0900

    Edited about license.

--- MinifyJpeg.js
+++ MinifyJpeg.js
@@ -1,20 +1,7 @@
-/* MinifyJpeg depends on jpg.js(https://github.com/notmasteryet/jpgjs)
-   jpg.js's license is below.
-
-   Copyright 2011 notmasteryet
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-
- * To minify a jpeg image without loosing EXIF.
+/* To minify a jpeg image without loosing EXIF.
  * TESTED(24/01/2013): FireFox, GoogleChrome, IE10, Opera
+ * Copyright (c) 2013 hMatoba
+ * Released under the MIT license
  *
  * To minify jpeg image:
  *     minified = MinifyJpeg.minify(image, length);
